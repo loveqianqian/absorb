f5f3d342dd79641927d274cd0f8a4a449d6b4ea1
loveqianqian <diaozhiwei2k@163.com>
2016-11-11 23:01:19 +0800

    init

--- src/main/java/com/qrplus/FileHelper.java
+++ src/main/java/com/qrplus/FileHelper.java
@@ -15,101 +15,22 @@ import java.util.Date;
  */
 public class FileHelper {
 
-    private static String myFileName = "";
-
-    public String getFileName(){
-        myFileName = "data" + getCurrentTime();
-        File file = new File(getAbsPath()+"/data");
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        return myFileName;
-    }
-
     public String getAdministratorPath() {
         FileSystemView fsv = FileSystemView.getFileSystemView();
         return String.valueOf(fsv.getHomeDirectory());
     }
 
-    public String getAbsPath() {
-        FileSystemView fsv = FileSystemView.getFileSystemView();
-        String administratorPath = String.valueOf(fsv.getHomeDirectory());
-        int i = administratorPath.lastIndexOf("\\");
-        return administratorPath.substring(0, i);
-    }
-
-    public void parseCode() throws IOException {
+    public void createMac() throws Exception {
         String administratorPath = getAdministratorPath();
-        File file = new File(administratorPath + "/data.txt");
-        File resultFile = new File(getAbsPath()+"/data/" + myFileName);
-        if (file.exists()) {
-            if (resultFile.exists()) {
-                resultFile.delete();
-                resultFile.createNewFile();
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
-            String line;
-            reader.readLine();
-            while ((line = reader.readLine()) != null) {
-                String temp = line.split("\\|\\|")[0];
-                writer.write(temp + "\n");
-                writer.flush();
-            }
-            reader.close();
-            writer.close();
-        }
-    }
-
-    public boolean solveManager() throws Exception {
+        String fileName = getCurrentTime();
         String code = new CodeHelper().getMacAddress();
-        String secretPath = getAdministratorPath() + "/manager/jre7/lib/amd64/config.properties";
-        String managePath=getAbsPath()+"/data/.manager.k";
-        if (isNotExistFile(secretPath)) {
-            saveConfig(code,secretPath);
-            saveMangeK(true, string2MD5(code),managePath);
-            return true;
-        }else{
-            BufferedReader reader1 = new BufferedReader(new InputStreamReader(new FileInputStream(new File(secretPath))));
-            String msg1 = reader1.readLine();
-            BufferedReader reader2 = new BufferedReader(new InputStreamReader(new FileInputStream(new File(managePath))));
-            String msg2 = reader2.readLine();
-            String msg3=string2MD5(code);
-            reader2.readLine();
-            String file = reader2.readLine();
-            File resultFile = new File(getAbsPath()+"/data/"+file);
-            resultFile.delete();
-            if (msg1.equals(msg2)&&msg1.equals(msg3)) {
-                saveMangeK(true,msg2,managePath);
-                return true;
-            }else{
-                saveMangeK(false,msg2,managePath);
-                return false;
-            }
+        String encrypt = string2MD5(code);
+        File file = new File(administratorPath + "/" + fileName + ".txt");
+        if (file.exists()) {
+            file.createNewFile();
         }
-    }
-
-    public boolean isExistFile(String fileName) {
-        File file = new File(fileName);
-        return file.exists();
-    }
-
-    public boolean isNotExistFile(String fileName) {
-        return !isExistFile(fileName);
-    }
-
-    public void saveMangeK(boolean flag, String mac,String path) throws Exception {
-        File file = new File(path);
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        writer.write(mac + "\n" + flag + "\n" + myFileName);
-        writer.flush();
-        writer.close();
-    }
-
-    public void saveConfig(String mac,String path) throws Exception {
-        File file = new File(path);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        writer.write(string2MD5(mac));
+        writer.write(encrypt);
         writer.flush();
         writer.close();
     }
--- src/main/java/com/qrplus/Straw.java
+++ src/main/java/com/qrplus/Straw.java
@@ -3,8 +3,11 @@ package com.qrplus;
 
 public class Straw {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("start...");
+        FileHelper helper = new FileHelper();
+        helper.createMac();
+        System.out.println("over...");
     }
 
 }
