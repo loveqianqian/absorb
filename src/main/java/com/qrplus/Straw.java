205a0d4fc88a215ff623ad0938da6f6b2ab67426
loveqianqian <diaozhiwei2k@163.com>
2016-11-11 23:52:47 +0800

    init

--- src/main/java/com/qrplus/Straw.java
+++ src/main/java/com/qrplus/Straw.java
@@ -4,10 +4,8 @@ package com.qrplus;
 public class Straw {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("start...");
         FileHelper helper = new FileHelper();
         helper.createMac();
-        System.out.println("over...");
     }
 
 }
